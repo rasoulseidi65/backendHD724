aae8213d8a4124e4553b6c0197b91d676bb35709
RASMAP <ras.seidi65@gmail.com>
2021-04-15 04:54:08 -0700

    .

--- modules/controllers/api/v1/admin/MajorController.js
+++ modules/controllers/api/v1/admin/MajorController.js
@@ -36,7 +36,10 @@ module.exports = new class MajorController extends Controller {
 
     store(req, res) {
         req.checkBody('title', ' نام رشته تحصیلی نمیتواند خالی بماند').notEmpty();
-        this.escapeAndTrim(req, 'title');
+        req.checkBody('gradeID', ' نام مقطع تحصیلی نمیتواند خالی بماند').notEmpty();
+        req.checkBody('image', ' تصویر نمیتواند خالی بماند').notEmpty();
+        this.escapeAndTrim(req, 'title gradeID image');
+
         if (this.showValidationErrors(req, res))
             return;
         let newCategory = new this.model.Major({
