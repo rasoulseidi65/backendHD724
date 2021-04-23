bd84ae1bee0815d98b5bf6de7cc6aa7628d7bcd3
RASMAP <ras.seidi65@gmail.com>
2021-04-23 00:30:25 -0700

    .

--- modules/controllers/api/v1/users/BasketController.js
+++ modules/controllers/api/v1/users/BasketController.js
@@ -6,7 +6,7 @@ module.exports = new class BasketController extends Controller {
         req.checkParams('id', 'ای دی وارد شده صحیح نیست').isMongoId();
         if (this.showValidationErrors(req, res))
             return;
-        this.model.Basket.find({userID:req.params.id}).populate('user Question Payment ').exec((err, cartcustom) => {
+        this.model.Basket.find({userID:req.params.id,success:'موفق'}).populate('user Question Payment ').exec((err, cartcustom) => {
             if (err) throw err;
             if (cartcustom) {
                 return res.json({
